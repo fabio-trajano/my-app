64e0b57454a3d57a479484a345801a227bed2884
Fabio Ramalho <fabiotrajano.eng@gmail.com>
2023-03-31 20:57:28 +0100

    import components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import Heading from './components/Heading';
+import Nav from './components/Nav';
+import Footer from './components/Footer';
+import Intro1 from './components/Intro1';
+import Intro2 from './components/Intro2';
+import Intro3 from './components/Intro3';
+import Promo from './components/Promo';
 
 function App() {
   return (
     <div className="App">
       <Heading />
+      <Nav />
+      <Intro1 />
+      <Intro2 />
+      <Intro3 />
+      <Promo />
+      <Footer />
     </div>
   );
 }
